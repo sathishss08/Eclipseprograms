1cd598864477598a915791d4273a42a1f5bcbfa8
sathishss08 <99020600+sathishss08@users.noreply.github.com>
2024-12-26 20:05:47 +0530

    Update compare.java

--- src/main/java/compare.java
+++ src/main/java/compare.java
@@ -15,17 +15,17 @@ public class compare {
 		for(int i=0 ;i<arr.length;i++)
 		{
 			
-			for(int j=0;j<arr.length-1-i;j++)
+			for(int j=i+1;j<arr.length;j++)
 			{
 				
 				
-			if(arr[j]>arr[j+1])
+			if(arr[i]>arr[j)
 			{
 				
-				temp=arr[j];
-				arr[j]=arr[j+1];
+				temp=arr[i];
+				arr[i]=arr[j];
 				
-				arr[j+1]=temp;
+				arr[j]=temp;
 			}
 			
 			
